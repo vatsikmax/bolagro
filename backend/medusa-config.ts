70d2c8bd79f99ed4c0b0abaaf91e7469cbe875dc
maksym-alavatskyi <maksym.alavatskyi@contentsquare.com>
2025-02-18 13:04:36 +0100

    add token api

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -4,7 +4,16 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
+    redisUrl: process.env.REDIS_URL,
     databaseUrl: process.env.DATABASE_URL,
+    databaseDriverOptions:
+      process.env.NODE_ENV !== 'development'
+        ? {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        }
+        : {},
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
